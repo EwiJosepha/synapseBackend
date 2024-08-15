ad6ba54de058bc5590b934a55dec839d67d7ead3
ewi josepha <ewijosepha@gmail.com>
2024-08-16 00:16:37 +0200

    implementing file attachment logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { MessageAuth } from './messages/messages.module';
 import { ConversationModule } from './conversation/conversation.module';
 import { ConversationParticipantModule } from './conversation-participant/con-participant.module';
 import { ReactionModule } from './message-reaction/mes-reaction.module';
+import { FileAttachmentModule } from './file-attachment/file-attachment.module';
 
 
 @Module({
-  imports: [AuthModule, MessageAuth, ConversationModule, ConversationParticipantModule, ReactionModule],
+  imports: [AuthModule, MessageAuth, ConversationModule, ConversationParticipantModule, ReactionModule, FileAttachmentModule],
   controllers: [],
   providers: [],
 })
