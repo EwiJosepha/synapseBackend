674d8f3281e97d017ff1db8db225fd66a3b27a71
ewi josepha <ewijosepha@gmail.com>
2024-08-15 22:28:56 +0200

    adding conversation participants logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { MessageAuth } from './messages/messages.module';
+import { ConversationModule } from './conversation/conversation.module';
 
 
 @Module({
-  imports: [AuthModule, MessageAuth],
+  imports: [AuthModule, MessageAuth, ConversationModule],
   controllers: [],
   providers: [],
 })
